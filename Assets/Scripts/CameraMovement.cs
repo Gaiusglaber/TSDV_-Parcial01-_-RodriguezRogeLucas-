59094e39759a0cf3c050f44b7914a60b11b786fc
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-05-17 15:53:10 -0300

    Init Proyect
    . Scenario added with basic walls and player location

---
+++ Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour
+{
+    [SerializeField]public GameObject player;
+    [SerializeField] [Range(0, 10)] public float camerax;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
