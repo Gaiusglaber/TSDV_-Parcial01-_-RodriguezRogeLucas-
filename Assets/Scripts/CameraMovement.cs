2d56e848d977ac55278356e23eaaee6813831c66
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-05-17 17:16:20 -0300

    CameraMovement and PlayerMovement added
    Camera movement follows player if player is not in the borders of the scenario

--- Assets/Scripts/CameraMovement.cs
+++ Assets/Scripts/CameraMovement.cs
@@ -4,16 +4,50 @@ using UnityEngine;
 
 public class CameraMovement : MonoBehaviour
 {
-    [SerializeField]public GameObject player;
-    [SerializeField] [Range(0, 10)] public float camerax;
+    private const float MINX = -12f;
+    private const float MAXX = 19f;
+    private const float MINZ = -14f;
+    private const float MAXZ = 21f;
+    [SerializeField] public GameObject player;
+    [SerializeField] [Range(-10, 10)] public float camerax;
+    [SerializeField] [Range(-10, 10)] public float cameray;
+    [SerializeField] [Range(-10, 10)] public float cameraz;
     void Start()
     {
         
     }
 
     // Update is called once per frame
-    void Update()
+    bool isInWidthScenario()
     {
+        return player.transform.position.x > MINX && player.transform.position.x<MAXX;
+    }
+
+    bool isInHeightScenario()
+    {
+        return player.transform.position.z < MAXZ && player.transform.position.z > MINZ;
+    }
+    void LateUpdate()
+    {
+        Vector3 pos = player.transform.position;
+        pos.x += camerax;
+        pos.y += cameray;
+        pos.z += cameraz;
+        if (isInWidthScenario() && isInHeightScenario())
+        {
+            transform.position = pos;
+        }
+        else if (!isInWidthScenario() && !isInHeightScenario())
+        {
+            transform.position = new Vector3(this.transform.position.x,pos.y, this.transform.position.z);
+        }
+        else if (!isInWidthScenario())
+        {
+            transform.position = new Vector3(this.transform.position.x,pos.y,pos.z);
+        }else if (!isInHeightScenario())
+        {
+            transform.position = new Vector3(pos.x, pos.y, this.transform.position.z);
+        }
         
     }
 }
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float speed = 5;
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y,
+                this.transform.position.z + speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.A))
+        {
+            this.transform.position = new Vector3(this.transform.position.x - speed * Time.deltaTime, this.transform.position.y,
+                this.transform.position.z);
+        }
+        else if (Input.GetKey(KeyCode.S))
+        {
+            this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y,
+                this.transform.position.z - speed * Time.deltaTime);
+        }
+        else if (Input.GetKey(KeyCode.D))
+        {
+            this.transform.position = new Vector3(this.transform.position.x + speed * Time.deltaTime, this.transform.position.y,
+                this.transform.position.z);
+        }
+    }
+}
